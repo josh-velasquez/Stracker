a98599b79b62176cbb2574eaf16d7901ebf3d09c
Josh Velasquez <joshuavelasquez@Joshs-MacBook-Pro.local>
2023-12-05 23:17:41 -0700

    Remove old code and updated readme

--- README.md
+++ README.md
@@ -2,7 +2,21 @@
 Stock tracker API that keeps tracks of your stocks using Node.js. The website provides a UI that allows you to check information on the target stocks using the [Yahoo Finance API](https://www.yahoofinanceapi.com/) and [Alpha Vantage API](https://polygon.io/stocks?gclid=EAIaIQobChMI_r-_zPW89gIVmz6tBh1sEA5PEAAYASAAEgKBr_D_BwE). Registering a stock and entering the target prices allows you to keep track of that stock through notified emails sent on intervals. If your target stock is below your target low price and/or your target stock is higher than your target high price, an email is sent to you indicating so.  
 Stracker's webpage also indicates the current time and a timer which indicates the time before the stock market closes.
 
+## Images
+![](images/homePage.png)
+
+![](images/trackerDropdown.png)
+
+![](images/sampleStock.png)
+
+
+## Links
+https://rapidapi.com/yourdevmail/api/yh-finance8
+
 __NOTE: Only the Yahoo Finance API is set up right now__
+
+## Tech stack
+Stracker utilizes NodeJS to send pages to the client. It also uses nodemailer to send emails to the user if the stock price for a targeted stock triggers the threshold assigned by the user.
 
 ## Running the server
 URL: [http://localhost:6060/](http://localhost:6060/)  
--- server/index.js
+++ server/index.js
@@ -200,31 +200,49 @@ function addUserNotification(userInfo) {
 //#endregion
 
 //#region Yahoo Finance
-const yhFinanceApiRootUrl = "https://yh-finance.p.rapidapi.com";
-const yhFinanceHost = "yh-finance.p.rapidapi.com";
+const yhFinanceApiRootUrl = "https://yh-finance8.p.rapidapi.com";
+const yhFinanceHost = "yh-finance8.p.rapidapi.com";
 const yhFinanceApiKey = config.yhFinanceApiKey;
+
+const sampleApplePayload = {
+  symbol: "AAPL",
+  price: {
+    shortName: "Apple Inc.",
+    regularMarketPrice: {
+      raw: "$2342.00",
+    },
+    regularMarketOpen: {
+      raw: "$12123123.00",
+    },
+    exchangeName: "AAPL",
+  },
+  summaryProfile: {
+    longBusinessSummary:
+      "Apple Inc. designs, manufactures, and markets smartphones, personal computers, tablets, wearables, and accessories worldwide. The company offers iPhone, a line of smartphones; Mac, a line of personal computers; iPad, a line of multi-purpose tablets; and wearables, home, and accessories comprising AirPods, Apple TV, Apple Watch, Beats products, and HomePod. It also provides AppleCare support and cloud services; and operates various platforms, including the App Store that allow customers to discover and download applications and digital content, such as books, music, video, games, and podcasts. In addition, the company offers various services, such as Apple Arcade, a game subscription service; Apple Fitness+, a personalized fitness service; Apple Music, which offers users a curated listening experience with on-demand radio stations; Apple News+, a subscription news and magazine service; Apple TV+, which offers exclusive original content; Apple Card, a co-branded credit card; and Apple Pay, a cashless payment service, as well as licenses its intellectual property. The company serves consumers, and small and mid-sized businesses; and the education, enterprise, and government markets. It distributes third-party applications for its products through the App Store. The company also sells its products through its retail and online stores, and direct sales force; and third-party cellular network carriers, wholesalers, retailers, and resellers. Apple Inc. was founded in 1976 and is headquartered in Cupertino, California.",
+  },
+};
 
 app.get("/yhfinance/stocks/summary", (req, res) => {
   console.log("Server Request: Yahoo Finance Summary");
   var symbol = req.query.symbol.toUpperCase();
   var options = {
     method: "GET",
-    url: yhFinanceApiRootUrl + "/stock/v2/get-summary",
+    url: yhFinanceApiRootUrl + "/stock/get_summary",
     params: { symbol: symbol, region: "US" },
     headers: {
       "x-rapidapi-host": yhFinanceHost,
       "x-rapidapi-key": yhFinanceApiKey,
     },
   };
-
-  axios
-    .request(options)
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  res.status(200).send(sampleApplePayload);
+  // axios
+  //   .request(options)
+  //   .then((response) => {
+  //     res.status(200).send(response.data.message);
+  //   })
+  //   .catch((error) => {
+  //     console.error(error.response.data.message);
+  //   });
 });
 
 //#endregion
